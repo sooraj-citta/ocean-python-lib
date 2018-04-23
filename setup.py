2a987c24d4f2975198725331ba9f7f1c1cc4314c
Sooraj S <38486383+sooraj-citta@users.noreply.github.com>
2018-04-23 17:39:24 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 23 11:42:29 2018
+
+@author: uvionics
+"""
+
+from setuptools import setup
+
+setup(name="citta",
+      version="0.0",
+      packages=["citta"])
