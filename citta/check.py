fd4cce7bb1703f2943aa6545f8abf585e2dfed78
Sooraj S <38486383+sooraj-citta@users.noreply.github.com>
2018-04-24 12:34:35 +0530

    Update check.py

--- citta/check.py
+++ citta/check.py
@@ -8,13 +8,12 @@ Created on Mon Apr 23 11:42:29 2018
 import csv
 from prettytable import from_csv
 
-class check:
-    def name(self):
-        """Returns the file name."""
-        return self.__name
+def name(self):
+    """Returns the file name."""
+    return self.__name
 
-    def summary(filename):
-        with open(filename,"r") as f:
-            x=from_csv(f)
-        print x
-        return x
+def summary(filename):
+    with open(filename,"r") as f:
+        x=from_csv(f)
+    print x
+    return x
